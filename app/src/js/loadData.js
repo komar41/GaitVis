dcef1e8e1d33e263231fb5bef12c04ad684eb1e4
komar3 <komar3@uic.edu>
2022-11-07 20:36:24 -0500

    data loaded

---
+++ app/src/js/loadData.js
@@ -0,0 +1,71 @@
+patient_id = "012518cm"
+
+d3.csv("data/".concat(patient_id, "/", patient_id, "step.csv")).then(data =>{
+    
+    // Patient step data. Process to extract step time, swing time, ... and so on
+
+})
+let parseSoc = () => {
+    return d3.csv("data/demongraphics.csv", data => {
+        // Converted some of the variables to integer type and changed the column names
+        return{
+            height: +data["Height (cm)"],
+            id: data.ID,
+            maxFootWidth: +data["Max foot width"],
+            weight: +data["Weight (Kg)"],
+            age: +data.age,
+            ankleWidth: +data["ankle width"],
+            asis: +data.asis,
+            footLength: +data["foot length"],
+            gender: +data["gender (0:F; 1:M)"],
+            heelWidth: +data["heel width"],
+            kneeWidth: +data["knee width"]
+        };
+    }).then(data =>{return data})
+}
+
+
+let parseGRF = (patient_id, fileName) => {
+    return d3.csv("data/".concat(patient_id, "/", patient_id, fileName), data => {
+        return{
+            time : +data.time,
+            rAP : +data["R-AP"],
+            lAP : +data["L-AP"],
+            rML : +data["R-ML"],
+            lML : +data["L-ML"],
+            rVT : +data["R-VT"],
+            lVT : +data["L-VT"]
+        }
+    }).then(data => {return data});
+}
+
+let parseJNT = (patient_id, fileName) => {
+    return d3.csv("data/".concat(patient_id, "/", patient_id, fileName), data => {
+        return{
+            time : +data.time,
+            rFoot : +data["Rfoot"],
+            lFoot : +data["Lfoot"],
+            rShank : +data["RShank"],
+            lShank : +data["LShank"],
+            rThigh : +data["Rthigh"],
+            lThigh : +data["Lthigh"],
+            trunk: +data["trunk"]
+        }
+    }).then(data => {return data});
+}
+
+async function fetchData(){
+    let soc = await parseSoc()
+
+    let grf22 = await parseGRF(patient_id, "_22_grf.csv");
+    let grf23 = await parseGRF(patient_id, "_23_grf.csv");
+    let grf24 = await parseGRF(patient_id, "_24_grf.csv");
+
+    let jnt22 = await parseJNT(patient_id, "_22_jnt.csv");
+    let jnt23 = await parseJNT(patient_id, "_23_jnt.csv");
+    let jnt24 = await parseJNT(patient_id, "_23_jnt.csv");
+
+    
+    // Call function required from your js file with the data you require 
+}
+fetchData()
---
+++ app/src/js/spider.js
@@ -0,0 +1,95 @@
+let data = [];
+let features = ["A","B","C","D","E","F"];
+//generate the data
+for (var i = 0; i < 3; i++){
+    var point = {}
+    //each feature will be a random number from 1-9
+    features.forEach(f => point[f] = 1 + Math.random() * 8);
+    data.push(point);
+}
+console.log(data);
+
+let svgSpider = d3.select("#div3").append("svg")
+    .attr("width", 350)
+    .attr("height", 350);
+
+let radialScale = d3.scaleLinear()
+    .domain([0,10])
+    .range([0,120]);
+let ticks = [2,4,6,8,10];
+
+ticks.forEach(t =>{
+    svgSpider.append("circle")
+    .attr("cx", 220)
+    .attr("cy", 150)
+    .attr("fill", "none")
+    .attr("stroke", "gray")
+    .attr("r", radialScale(t))
+    console.log(radialScale(t))
+}    
+);
+
+ticks.forEach(t =>
+    svgSpider.append("text")
+    .attr("x", 225)
+    .attr("y", 150 - radialScale(t))
+    .text(t.toString())
+);
+
+function angleToCoordinate(angle, value){
+    let x = Math.cos(angle) * radialScale(value);
+    let y = Math.sin(angle) * radialScale(value);
+    return {"x": 220 + x, "y": 150 - y};
+}
+
+for (var i = 0; i < features.length; i++) {
+    let ft_name = features[i];
+    let angle = (Math.PI / 2) + (2 * Math.PI * i / features.length);
+    let line_coordinate = angleToCoordinate(angle, 10);
+    let label_coordinate = angleToCoordinate(angle, 10.5);
+
+    //draw axis line
+    svgSpider.append("line")
+    .attr("x1", 220)
+    .attr("y1", 150)
+    .attr("x2", line_coordinate.x)
+    .attr("y2", line_coordinate.y)
+    .attr("stroke","black");
+
+    //draw axis label
+    svgSpider.append("text")
+    .attr("x", label_coordinate.x - 5)
+    .attr("y", label_coordinate.y + 3)
+    .text(ft_name);
+}
+
+let line = d3.line()
+    .x(d => d.x)
+    .y(d => d.y);
+let colors = ["darkorange", "gray", "navy"];
+
+function getPathCoordinates(data_point){
+    let coordinates = [];
+    for (var i = 0; i < features.length; i++){
+        let ft_name = features[i];
+        let angle = (Math.PI / 2) + (2 * Math.PI * i / features.length);
+        coordinates.push(angleToCoordinate(angle, data_point[ft_name]));
+    }
+    return coordinates;
+}
+
+for (var i = 0; i < data.length; i ++){
+    let d = data[i];
+    let color = colors[i];
+    let coordinates = getPathCoordinates(d);
+
+    //draw the path element
+    svgSpider.append("path")
+    .datum(coordinates)
+    .attr("d",line)
+    .attr("stroke-width", 3)
+    .attr("stroke", color)
+    .attr("fill", color)
+    .attr("stroke-opacity", 1)
+    .attr("opacity", 0.5);
+}
