3e0b208b5a043709f2143139eb18000a94f0add7
komar3 <komar3@uic.edu>
2022-11-08 21:32:00 -0500

    paracoord

--- app/src/js/paraCoord.js
+++ app/src/js/paraCoord.js
@@ -1,5 +1,5 @@
-const margin = {top: 30, right: 50, bottom: 10, left: 50},
-  width = 600 - margin.left - margin.right,
+const margin = {top: 30, right: 50, bottom: 10, left: 40},
+  width = 480 - margin.left - margin.right,
   height = 400 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
@@ -12,25 +12,27 @@ const svg = d3.select("#pcoordViz")
         `translate(${margin.left},${margin.top})`);
 
 // Parse the Data
-d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/iris.csv").then( function(data) {
+d3.csv("data/all_trials.csv").then( function(data) {
 
   console.log(data)
   // Color scale: give me a specie name, I return a color
   const color = d3.scaleOrdinal()
-    .domain(["stroke-survivor patients", "healthy-older adults"])
-    .range([ "#440154ff", "#21908dff", "#fde725ff"])
+    .domain(["stroke-survivor", "older-healthy" ])
+    .range(["#21908dff", "#fde725ff"])
 
   // Here I set the list of dimension manually to control the order of axis:
-  dimensions = ["Step Time (L)", "Swing Time (L)", "Stride Time (L)", "Step Time (R)", "Swing Time (R)", "Stride Time (R)"]
+  dimensions = ["Step Time (L)", "Step Time (R)", "Stride Time (L)", "Stride Time (R)", "Swing Time (L)", "Swing Time (R)"]
 
   // For each dimension, I build a linear scale. I store all in a y object
   const y = {}
   for (i in dimensions) {
-    name = dimensions[i]
-    y[name] = d3.scaleLinear()
+    n = dimensions[i]
+    y[n] = d3.scaleLinear()
       .domain( [0,8] ) // --> Same axis range for each group
       // --> different axis range for each group --> .domain( [d3.extent(data, function(d) { return +d[name]; })] )
       .range([height, 0])
+
+    console.log(y[n])
   }
 
   // Build the X scale -> it find the best position for each Y axis
@@ -41,7 +43,7 @@ d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/ir
   // Highlight the specie that is hovered
   const highlight = function(event, d){
 
-    selected_specie = d.Species
+    selected_specie = d.patient_type
 
     // first every group turns grey
     d3.selectAll(".line")
@@ -59,7 +61,7 @@ d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/ir
   const doNotHighlight = function(event, d){
     d3.selectAll(".line")
       .transition().duration(200).delay(1000)
-      .style("stroke", function(d){ return( color(d.Species))} )
+      .style("stroke", function(d){ return( color(d.patient_type))} )
       .style("opacity", "1")
   }
 
@@ -73,10 +75,10 @@ d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/ir
     .selectAll("myPath")
     .data(data)
     .join("path")
-      .attr("class", function (d) { return "line " + d.Species } ) // 2 class for each line: 'line' and the group name
+      .attr("class", function (d) { return "line " + d.patient_type } ) // 2 class for each line: 'line' and the group name
       .attr("d",  path)
       .style("fill", "none" )
-      .style("stroke", function(d){ return( color(d.Species))} )
+      .style("stroke", function(d){ return( color(d.patient_type))} )
       .style("opacity", 0.5)
       .on("mouseover", highlight)
       .on("mouseleave", doNotHighlight )
@@ -90,12 +92,13 @@ d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/ir
     // I translate this element to its right position on the x axis
     .attr("transform", function(d) { return `translate(${x(d)})`})
     // And I build the axis with the call function
-    .each(function(d) { d3.select(this).call(d3.axisLeft().ticks(5).scale(y[d])); })
+    .each(function(d) { d3.select(this).call(d3.axisLeft().ticks(5).scale(y[d])).style("font-size", "12").style("font-weight", "bold"); })
     // Add axis title
     .append("text")
       .style("text-anchor", "middle")
       .attr("y", -9)
       .text(function(d) { return d; })
       .style("fill", "black")
-
-})
+      .style("font-size", 12)
+      .style("font-weight", "bold")
+    })
