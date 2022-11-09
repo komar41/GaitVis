dce8f822fa952964b129005d1b55149f354a6059
Sajal Kherde <skherde1@gmail.com>
2022-11-09 01:18:12 -0600

    GRF graphs

--- app/src/js/lineGRF.js
+++ app/src/js/lineGRF.js
@@ -1,8 +1,8 @@
-var marginGFR = { top: 10, right: 10, bottom: 50, left: 50 },
+var marginGFR = { top: 10, right: 10, bottom: 50, left: 30 },
     widthGFR = 500 - marginGFR.left - marginGFR.right,
     heightGFR = 300 - marginGFR.top - marginGFR.bottom;
 
-var svgGFR = d3.select("#div4")
+var svgGFRLeft = d3.select("#div4")
     .append("svg")
     .attr("width", widthGFR + marginGFR.left + marginGFR.right)
     .attr("height", heightGFR + marginGFR.top + marginGFR.bottom)
@@ -10,34 +10,95 @@ var svgGFR = d3.select("#div4")
     .attr("transform",
         "translate(" + marginGFR.left + "," + marginGFR.top + ")");
 
-// define the line
-var valueline = d3.line()
-    .x(function (d) { return x(d.time); })
-    .y(function (d) { return y(d.rap); });
+var svgGFRRight = d3.select("#div5")
+    .append("svg")
+    .attr("width", widthGFR + marginGFR.left + marginGFR.right)
+    .attr("height", heightGFR + marginGFR.top + marginGFR.bottom)
+    .append("g")
+    .attr("transform",
+        "translate(" + marginGFR.left + "," + marginGFR.top + ")");
 
-// Get the data
 d3.csv("data/012518cm/012518cm_22_grf.csv").then(
     function (data) {
         var x = d3.scaleLinear()
             .domain([0, d3.max(data, function (d) { return +d.time; })])
             .range([0, widthGFR]);
-        svgGFR.append("g")
+        svgGFRLeft.append("g")
+            .attr("transform", "translate(0," + heightGFR + ")")
+            .call(d3.axisBottom(x));
+        svgGFRRight.append("g")
             .attr("transform", "translate(0," + heightGFR + ")")
             .call(d3.axisBottom(x));
 
-        var y = d3.scaleLinear()
-            .domain([d3.min(data, function (d) { return +d.rap; }), d3.max(data, function (d) { return +d.rap; })])
+        var yleft = d3.scaleLinear()
+            .domain([d3.min(data, function (d) { return Math.min(d.lap, d.lml, d.lvt); }), d3.max(data, function (d) { return Math.max(d.lap, d.lml, d.lvt); })])
             .range([heightGFR, 0]);
-        svgGFR.append("g")
-            .call(d3.axisLeft(y));
+        svgGFRLeft.append("g")
+            .call(d3.axisLeft(yleft));
 
-        svgGFR.append("path")
+        var yright = d3.scaleLinear()
+            .domain([d3.min(data, function (d) { return Math.min(d.rap, d.rml, d.rvt); }), d3.max(data, function (d) { return Math.max(d.rap, d.rml, d.rvt); })])
+            .range([heightGFR, 0]);
+        svgGFRRight.append("g")
+            .call(d3.axisLeft(yright));
+
+        svgGFRLeft.append("path")
             .datum(data)
             .attr("fill", "none")
             .attr("stroke", "steelblue")
             .attr("stroke-widthGFR", 2)
             .attr("d", d3.line()
                 .x(function (d) { return x(d.time) })
-                .y(function (d) { return y(d.rap) })
+                .y(function (d) { return yleft(d.lap) })
+            )
+
+        svgGFRLeft.append("path")
+            .datum(data)
+            .attr("fill", "none")
+            .attr("stroke", "red")
+            .attr("stroke-widthGFR", 2)
+            .attr("d", d3.line()
+                .x(function (d) { return x(d.time) })
+                .y(function (d) { return yleft(d.lml) })
+            )
+
+        svgGFRLeft.append("path")
+            .datum(data)
+            .attr("fill", "none")
+            .attr("stroke", "green")
+            .attr("stroke-widthGFR", 2)
+            .attr("d", d3.line()
+                .x(function (d) { return x(d.time) })
+                .y(function (d) { return yleft(d.lvt) })
+            )
+
+        svgGFRRight.append("path")
+            .datum(data)
+            .attr("fill", "none")
+            .attr("stroke", "steelblue")
+            .attr("stroke-widthGFR", 2)
+            .attr("d", d3.line()
+                .x(function (d) { return x(d.time) })
+                .y(function (d) { return yleft(d.rap) })
+            )
+
+        svgGFRRight.append("path")
+            .datum(data)
+            .attr("fill", "none")
+            .attr("stroke", "red")
+            .attr("stroke-widthGFR", 2)
+            .attr("d", d3.line()
+                .x(function (d) { return x(d.time) })
+                .y(function (d) { return yleft(d.rml) })
+            )
+
+        svgGFRRight.append("path")
+            .datum(data)
+            .attr("fill", "none")
+            .attr("stroke", "green")
+            .attr("stroke-widthGFR", 2)
+            .attr("d", d3.line()
+                .x(function (d) { return x(d.time) })
+                .y(function (d) { return yleft(d.rvt) })
             )
     });
