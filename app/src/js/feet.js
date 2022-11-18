e24bc877765179d3f5d3dd9978197bcc2274833d
Soham Pradhan <spradh8@uic.edu>
2022-11-17 22:17:25 -0600

    adding svg files

--- app/src/js/feet.js
+++ app/src/js/feet.js
@@ -1,10 +1,10 @@
 var svg_left =  d3.select("#leftFoot").append("img")
-    .attr("src","http://localhost:8888/Downloads/left_foot.svg")
+    .attr("src","/Desktop/GaitVis/app/left_foot.svg")
     .attr("width", 210)
     .attr("height", 210)
 
     var svg_right =  d3.select("#rightFoot").append("img")
-    .attr("src","http://localhost:8888/Downloads/right_foot.svg")
+    .attr("src","/Desktop/GaitVis/app/right_foot.svg")
     .attr("width", 230)
     .attr("height", 230)
 
