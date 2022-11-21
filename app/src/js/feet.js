77350b2f9dabe0168d57fc091277a03db39efb3a
Soham Pradhan <spradh8@uic.edu>
2022-11-20 18:15:39 -0600

    slider and svg connected
    slider and svg have been connected, database linking left

--- app/src/js/feet.js
+++ app/src/js/feet.js
@@ -1,35 +1,8 @@
-//  var svg_left =  d3.select("#leftFoot").append("img")
-//      .attr("src","/Desktop/GaitVis/app/left_foot.svg")
-//      .attr("width", 210)
-//      .attr("height", 210)
+//AP colors -> (178,226,226), (102,194,164), (35,139,69)
+//ML colors -> (203,201,226), (158,154,200), (106,81,163)
+//VT colors -> (252,174,145), (251,106,74), (203,24,29)
 
-// var svg_right =  d3.select("#rightFoot").append("img")
-//     .attr("src","/Desktop/GaitVis/app/right_foot.svg")
-//     .attr("width", 230)
-//     .attr("height", 230)
-
-// d3.select('#rightFoot')
-//     .append('circle')
-//         .attr('cx', 10)
-//         .attr('cy', 10)
-//         .attr('r', 20)
-//         .style('fill','blue')
-var colorss = [ 'rgb(44,162,95)','rgb(153,216,201)','rgb(229,245,249)','rgb(224,236,244)', 'rgb(158,188,218)','rgb(136,86,167)','rgb(158,188,218)','rgb(224,236,244)','rgb(255,237,160)', 'rgb(254,178,76)','rgb(240,59,32)' ];
-
-var grad = svg.append('defs')
-  .append('linearGradient')
-  .attr('id', 'grad')
-  .attr('x1', '100%')
-  .attr('x2', '0%');
-
-grad.selectAll('stop')
-  .data(colorss)
-  .enter()
-  .append('stop')
-  .style('stop-color', function(d){ return d; })
-  .attr('offset', function(d,i){
-    return 100 * (i / (colorss.length - 1)) + '%';
-  })
+//Base svgs
 
 var svg_left = d3.select("#leftFoot")
              .append("svg")
@@ -43,147 +16,238 @@ var svg_right = d3.select("#rightFoot")
              .attr("height", 210)
              .style('background-color', 'white')
 
+//Circle svgs
 
-
-var circle = svg_left.append("circle")
+svg_left.append("circle")
             .attr("cx", 140)
             .attr("cy", 30)
             .attr("r", 14)
             .attr("fill",'black');
 
-var circle = svg_left.append("circle")
+svg_left.append("circle")
             .attr("cx", 112)
             .attr("cy", 35)
             .attr("r", 12)
             .attr("fill",'black');
 
-var circle = svg_left.append("circle")
+svg_left.append("circle")
             .attr("cx", 95)
             .attr("cy", 52)
             .attr("r", 10)
             .attr("fill",'black');
             
-var circle = svg_left.append("circle")
+svg_left.append("circle")
             .attr("cx", 88)
             .attr("cy", 70)
             .attr("r", 8)
-            .attr("fill",'black');           
+            .attr("fill",'black');  
 
-var circle = svg_left.append("circle")
+svg_left.append("circle")
             .attr("cx", 86)
             .attr("cy", 86)
             .attr("r", 6)
-            .attr("fill",'black');   
+            .attr("fill",'black');  
 
-var ellipse = svg_left.append("ellipse")
-            .attr("cx", 103)
-            .attr("cy", 110)
-            .attr("rx", 78)
-            .attr("ry", 38)
-            .attr("transform", `translate(240,20) rotate(90)`);
-
-var circle = svg_right.append("circle")
+svg_right.append("circle")
             .attr("cx", 80)
             .attr("cy", 30)
             .attr("r", 14)
             .attr("fill",'black');
 
-var circle = svg_right.append("circle")
+svg_right.append("circle")
             .attr("cx", 108)
             .attr("cy", 35)
             .attr("r", 12)
             .attr("fill",'black');
 
-var circle = svg_right.append("circle")
+svg_right.append("circle")
             .attr("cx", 123)
             .attr("cy", 55)
             .attr("r", 10)
             .attr("fill",'black');
             
-var circle = svg_right.append("circle")
+svg_right.append("circle")
             .attr("cx", 130)
             .attr("cy", 75)
             .attr("r", 8)
             .attr("fill",'black');           
 
-var circle = svg_right.append("circle")
+svg_right.append("circle")
             .attr("cx", 133)
             .attr("cy", 90)
             .attr("r", 6)
-            .attr("fill",'black');   
+            .attr("fill",'black');
 
+//Feet SVG (Semi circle + 3 rectangles for every side)
 
+const arcGenerator = d3.arc()
+            .outerRadius(40)
+            .innerRadius(0)
+            .startAngle(-Math.PI / 2)
+            .endAngle(Math.PI / 2);
 
-var ellipse2 = svg_right.append("ellipse")
-            .attr("id","temp")
-            .attr("cx", 103)
-            .attr("cy", 150)
-            .attr("rx", 78)
-            .attr("ry", 38)
-            .attr("fill", 'url(#grad)')
-            .attr("transform", `translate(240,20) rotate(90)`);
-var a = 0;
+//Left feet
+
+//Arc 1
+svg_left.append("path")
+            .attr('id', 'temp1')
+            .attr("transform", "translate(140,90)")
+            .attr("d", arcGenerator())
+            .attr("fill", 'rgb(178,226,226)');
+
+//Rectangle 1
+svg_left.append("rect")
+              .attr('id', 'temp2')
+              .attr("x",100)
+              .attr("y", 90)
+              .attr("width", 26)
+              .attr("height", 70)
+              .attr("fill", 'rgb(203,201,226)');
+
+//Rectangle 2
+svg_left.append("rect")
+              .attr('id', 'temp3')
+              .attr("x",126)
+              .attr("y", 90)
+              .attr("width", 28)
+              .attr("height", 70)
+              .attr("fill", 'rgb(252,174,145)');
+
+//Rectangle 3
+svg_left.append("rect")
+              .attr('id', 'temp4')
+              .attr("x",154)
+              .attr("y", 90)
+              .attr("width", 26)
+              .attr("height", 70)
+              .attr("fill", 'rgb(203,201,226)');
+//Arc 2
+svg_left.append("path")
+                .attr('id', 'temp5')
+                .attr("transform", "translate(140,160) rotate(180)")
+                .attr("d", arcGenerator())
+                .attr("fill", 'rgb(178,226,226)');
+//Right feet
+
+//Arc 1
+svg_right.append("path")
+            .attr('id', 'temp6')
+            .attr("transform", "translate(80,90)")
+            .attr("d", arcGenerator())
+            .attr("fill", 'rgb(178,226,226)');
+
+//Rectangle 1
+svg_right.append("rect")
+                .attr('id', 'temp7')
+                .attr("x",40)
+                .attr("y", 90)
+                .attr("width", 26)
+                .attr("height", 70)
+                .attr("fill", 'rgb(203,201,226)');
+      
+//Rectangle 2
+svg_right.append("rect")
+                .attr('id', 'temp8')
+                .attr("x",66)
+                .attr("y", 90)
+                .attr("width", 28)
+                .attr("height", 70)
+                .attr("fill", 'rgb(252,174,145)');
+
+//Rectangle 3
+svg_right.append("rect")
+                .attr('id', 'temp9')
+                .attr("x",94)
+                .attr("y", 90)
+                .attr("width", 26)
+                .attr("height", 70)
+                .attr("fill", 'rgb(203,201,226)');
+
+//Arc 2
+svg_right.append("path")
+                .attr('id', 'temp10')
+                .attr("transform", "translate(80,160) rotate(180)")
+                .attr("d", arcGenerator())
+                .attr("fill", 'rgb(178,226,226)');
+
 function updateSlice(val){
-  
     for(var i =  0; i < data.length ; i++){
-            a+=1
-            if(a%2 == 0){
-              d3.select("#temp").remove();
-              colorss[0] = 'rgb(153,216,201)';
-              var grad = svg.append('defs')
-                .append('linearGradient')
-                .attr('id', 'grad')
-                .attr('x1', '100%')
-                .attr('x2', '0%');
 
-              grad.selectAll('stop')
-                .data(colorss)
-                .enter()
-                .append('stop')
-                .style('stop-color', function(d){ return d; })
-                .attr('offset', function(d,i){
-                  return 100 * (i / (colorss.length - 1)) + '%';
-              })
-              
-              var ellipse3 = svg_right.append("ellipse")
-                .attr("id","234")
-                .attr("cx", 103)
-                .attr("cy", 150)
-                .attr("rx", 78)
-                .attr("ry", 38)
-                .attr("fill", 'url(#grad)')
-                .attr("transform", `translate(240,20) rotate(90)`);
-              console.log(val);
-              console.log(colorss);
-            }
-            else{
-              d3.select("#temp").remove();
-              colorss[0] = 'rgb(44,162,95)';
-              var grad = svg.append('defs')
-                .append('linearGradient')
-                .attr('id', 'grad')
-                .attr('x1', '100%')
-                .attr('x2', '0%');
+      //Left AP
+      if(val < 33){
+        d3.select("#temp1").attr("fill",'rgb(178,226,226)');
+        d3.select("#temp5").attr("fill",'rgb(178,226,226)');
+      }
+      if(val > 33 && val < 66){
+        d3.select("#temp1").attr("fill",'rgb(102,194,164)');
+        d3.select("#temp5").attr("fill",'rgb(102,194,164)');
+      }
+      if(val > 66){
+        d3.select("#temp1").attr("fill",'rgb(35,139,69)');
+        d3.select("#temp5").attr("fill",'rgb(35,139,69)');
+      }
 
-              grad.selectAll('stop')
-                .data(colorss)
-                .enter()
-                .append('stop')
-                .style('stop-color', function(d){ return d; })
-                .attr('offset', function(d,i){
-                  return 100 * (i / (colorss.length - 1)) + '%';
-               })
-              var ellipse4 = svg_right.append("ellipse")
-                .attr("id","234")
-                .attr("cx", 103)
-                .attr("cy", 150)
-                .attr("rx", 78)
-                .attr("ry", 38)
-                .attr("fill", 'url(#grad)')
-                .attr("transform", `translate(240,20) rotate(90)`);
-                console.log(val);
-                console.log(colorss);
-            }
-        }
+      //Right AP
+      if(val < 33){
+        d3.select("#temp6").attr("fill",'rgb(178,226,226)');
+        d3.select("#temp10").attr("fill",'rgb(178,226,226)');
+      }
+      if(val > 33 && val < 66){
+        d3.select("#temp6").attr("fill",'rgb(102,194,164)');
+        d3.select("#temp10").attr("fill",'rgb(102,194,164)');
+      }
+      if(val > 66){
+        d3.select("#temp6").attr("fill",'rgb(35,139,69)');
+        d3.select("#temp10").attr("fill",'rgb(35,139,69)');
+      }
+
+      //Left ML
+      if(val < 33){
+        d3.select("#temp2").attr("fill",'rgb(203,201,226)');
+        d3.select("#temp4").attr("fill",'rgb(203,201,226)');
+      }
+      if(val > 33 && val < 66){
+        d3.select("#temp2").attr("fill",'rgb(158,154,200)');
+        d3.select("#temp4").attr("fill",'rgb(158,154,200)');
+      }
+      if(val > 66){
+        d3.select("#temp2").attr("fill",'rgb(106,81,163)');
+        d3.select("#temp4").attr("fill",'rgb(106,81,163)');
+      }
+      
+      //Right ML
+      if(val < 33){
+        d3.select("#temp7").attr("fill",'rgb(203,201,226)');
+        d3.select("#temp9").attr("fill",'rgb(203,201,226)');
+      }
+      if(val > 33 && val < 66){
+        d3.select("#temp7").attr("fill",'rgb(158,154,200)');
+        d3.select("#temp9").attr("fill",'rgb(158,154,200)');
+      }
+      if(val > 66){
+        d3.select("#temp7").attr("fill",'rgb(106,81,163)');
+        d3.select("#temp9").attr("fill",'rgb(106,81,163)');
+      }
+
+      //Left VT
+      if(val < 33){
+        d3.select("#temp3").attr("fill",'rgb(252,174,145)');
+      }
+      if(val > 33 && val < 66){
+        d3.select("#temp3").attr("fill",'rgb(251,106,74)');
+      }
+      if(val > 66){
+        d3.select("#temp3").attr("fill",'rgb(203,24,29)');
+      }
+      //Right VT
+      if(val < 33){
+        d3.select("#temp8").attr("fill",'rgb(252,174,145)');
+      }
+      if(val > 33 && val < 66){
+        d3.select("#temp8").attr("fill",'rgb(251,106,74)');
+      }
+      if(val > 66){
+        d3.select("#temp8").attr("fill",'rgb(203,24,29)');
+      }
+    }
 }
-
